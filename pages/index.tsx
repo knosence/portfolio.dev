f72288e2e71b34d69757c068a5292def63fc2014
Knosence <nadario.seays@gmail.com>
2023-04-12 22:42:35 -0400

    addes about section with id and snap-center, changed hero to snap-top

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import type { NextPage } from "next";
@@ -24,11 +25,14 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
       {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
 
