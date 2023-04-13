c1318b82ffbcb97ba38507baf598cf0f691a1d1d
Knosence <nadario.seays@gmail.com>
2023-04-12 23:20:54 -0400

    updated size of typewriter text. Phone had weird bug.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 type Props = {};
 
-export default function Hero({}: Props) {
+export default function Hero({ }: Props) {
   const [text, count] = useTypewriter({
     words: ["Hi👋, I'm NaDario Seays", "Welcome to my Portfolio"],
     loop: true,
@@ -25,7 +25,7 @@ export default function Hero({}: Props) {
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Web Developer
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-3xl md:text-5xl lg:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
