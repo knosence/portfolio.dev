f9f328255cf03b006e60b478daa6fd8314424c0d
Knosence <nadario.seays@gmail.com>
2023-04-12 21:11:56 -0400

    changed what is said on the hero, and size and added icons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,12 +6,7 @@ type Props = {};
 
 export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
-    words: [
-      "Hi, My name is NaDario Seays",
-      "I code like a hacker and drink coffee like a wizard.",
-      "I don't always code, but when I do, I prefer Linux.",
-      "Also BTW, I use Vim!!",
-    ],
+    words: ["Hi👋, My name is NaDario Seays!!", "Welcome to my Portfolio!!"],
     loop: true,
     delaySpeed: 2000,
   });
@@ -21,14 +16,19 @@ export default function Hero({}: Props) {
       <BackgroundCircles />
       {/*TODO: Add a image properly*/}
       <img
-        className="relative rounded-full h-32 w-32 mx-auto"
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src="https://lh3.googleusercontent.com/pw/AJFCJaVb3KUmYH0ZTVOnUjuJ_dgwNX_cQY-tU3HIdCpJyZMn6-eVGA9JVkufyrc1_Unad8jmN7EMp9_3lHegBBn450Fp4l1sJLn3ijI8l4ydPbSFPDIur3EsNjWqrMXe5-6x2dQVeF31Q0hEeXnBXkR4kSRl=w902-h953-s-no?authuser=0"
-        alt=""
+        alt="My Profile Picture"
       />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Web Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold scrol-px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+      </div>
     </div>
   );
 }
