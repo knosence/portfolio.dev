889bb20b87a2d9b081d3b0dfe3c92329cff76a06
Knosence <nadario.seays@gmail.com>
2023-04-15 17:44:32 -0400

    forgot to push when last in.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,11 +3,30 @@ import React from "react";
 type Props = {};
 
 function Projects({}: Props) {
+  const projects = [1, 2, 3, 4, 5];
   return (
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-schreen">
+            <img src="../images/portfolio-profile.png" alt="" />
+
+            <div>
+              <h4>
+                <span className="underline decoration-[#F7AB0A]/50">
+                  Case Study {i + 1} of {projects.length}:
+                </span>{" "}
+                UPS clone
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
+
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
     </div>
   );
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,23 +4,21 @@ import Skill from "./Skill";
 
 type Props = {};
 
-function Skills({ }: Props) {
+function Skills({}: Props) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
-      className="flex relative flex-col text-center md:text-left 
-    xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center 
-    xl:space-y-0 mx-auto items-center"
+      transition={{ duration: 1 }}
+      className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] 
+      xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
 
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-        Skills listed are ones I have a good understanding of and feel confident
-        with doing projects
+        Hover over a skill to see my proficiency level
       </h3>
 
       <div className="grid grid-cols-4 gap-5">
