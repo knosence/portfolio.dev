7a9399406e60e3869a10071fc3466c32fe602397
Knosence <nadario.seays@gmail.com>
2023-04-15 20:03:06 -0400

    removed projects from map para,

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,7 @@ function Projects({}: Props) {
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-        {projects.map((project, i) => (
+        {projects.map((i) => (
           <motion.div
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center 
           justify-center p-20 md:p-44 h-schreen"
