765ec0df190d639860b0dfeda89e42cb6355b153
Knosence <nadario.seays@gmail.com>
2023-04-16 03:30:15 -0400

    This should do it

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,35 +21,33 @@ function Projects({}: Props) {
         className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 
       scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb[#815af5]/80"
       >
-        {projects.map((i) => (
-          <div
-            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center 
+        <div
+          className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center 
           justify-center p-20 md:p-44 h-screen"
-          >
-            <motion.img
-              initial={{ y: -300, opacity: 0 }}
-              transition={{ duration: 1.2 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              src="images/portfolio-profile.png"
-              alt="Picture of the author"
-              className="border border-gray-500 w-[auto] h-[400px]"
-            />
-            <div className="space-y-10 px-0 md:px-10 max-w-6x1">
-              <h4 className="text-4xl fold-semibold text-center">
-                <span className="underline decoration-[#815af5]/50">
-                  Case Study of {projects.length}:
-                </span>{" "}
-                UPS clone
-              </h4>
-              <p className="text-lg text-center md:text-left">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-              </p>
-            </div>
+        >
+          <motion.img
+            initial={{ y: -300, opacity: 0 }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            src="images/portfolio-profile.png"
+            alt="Picture of the author"
+            className="border border-gray-500 w-[auto] h-[400px]"
+          />
+          <div className="space-y-10 px-0 md:px-10 max-w-6x1">
+            <h4 className="text-4xl fold-semibold text-center">
+              <span className="underline decoration-[#815af5]/50">
+                Case Study of {projects.length}:
+              </span>{" "}
+              UPS clone
+            </h4>
+            <p className="text-lg text-center md:text-left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            </p>
           </div>
-        ))}
+        </div>
       </div>
 
       <div className="w-full absolute top-[30%] bg-[#815af5]/10 left-0 h-[500px] -skew-y-12" />
